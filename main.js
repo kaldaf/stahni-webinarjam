8c6a2e3f09e03e05d32fb7f30f07309c22d59af0
moias <jsem@filipkalousek.cz>
2022-02-28 10:01:42 +0100

    Popup message

--- main.js
+++ main.js
@@ -19,9 +19,54 @@
         window.open(src, "_blank");
     }
 
-    chrome.runtime.onMessage.addListener((message) => {
-        if (message === "webinar-run") {
+    function isEnd() {
+        const isEnd = window.location.pathname.includes('replay')
+        if (isEnd) {
             newTabVideo();
+            createPopUpMessage('Úspěšně uloženo do clipboardu.', 5500)
+        } else {
+            createPopUpMessage('Video není připraveno.', 3500)
+        }
+    }
+
+    function createPopUpMessage(messsage, delay) {
+        const popUp = document.createElement('div');
+        popUp.innerHTML = messsage;
+
+        popUp.style.background = "#00fffb";
+        popUp.style.position = "fixed";
+        popUp.style.bottom = "-5rem";
+        popUp.style.left = "50%";
+        popUp.style.transform = "translateX(-50%)";
+        popUp.style.color = "#000";
+        popUp.style.padding = "1rem";
+        popUp.style.border = "3px solid #000";
+        popUp.style.fontWeight = "bold";
+        popUp.style.fontSize = "16px";
+        popUp.style.transition = "all 0.2s ease-in-out";
+        popUp.style.zIndex = "11111111";
+
+        document.body.append(popUp);
+
+        setTimeout(() => {
+            popUp.style.bottom = "2rem";
+        }, 100);
+
+
+        setTimeout(() => {
+            popUp.style.bottom = "-5rem";
+        }, delay);
+
+        setTimeout(() => {
+            popUp.remove();
+        }, (delay + 200));
+    }
+
+    chrome.runtime.onMessage.addListener((message) => {
+        if (message === "is-end") {
+            isEnd();
+        } else {
+            createPopUpMessage('Nastala chyba.', 3500)
         }
     });
 })();
--- popup.js
+++ popup.js
@@ -1,11 +1,19 @@
-document.querySelector('button').addEventListener('click', () => {
-    chrome.tabs.query({
-        currentWindow: true,
-        active: true
-    }, (tabs) => {
-        chrome.tabs.sendMessage(tabs[0].id, 'webinar-run');
-    })
-    chrome.tabs.executeScript({
-        file: "main.js"
+document.addEventListener('DOMContentLoaded', (e) => {
+    const button = document.querySelector('--btn-download-webinarjam');
+    setupButton(button);
+
+})
+
+const setupButton = (element) => {
+    document.querySelector('button').addEventListener('click', () => {
+        chrome.tabs.query({
+            currentWindow: true,
+            active: true
+        }, (tabs) => {
+            chrome.tabs.sendMessage(tabs[0].id, 'is-end');
+        })
+        chrome.tabs.executeScript({
+            file: "main.js"
+        });
     });
-});
+}
