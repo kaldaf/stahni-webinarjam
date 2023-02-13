073b9a0be8bb90858983d0f6edcb4288c01563b2
moias <jsem@filipkalousek.cz>
2023-02-13 10:52:10 +0100

    fix: manifest v3

--- popup.js
+++ popup.js
@@ -1,19 +1,83 @@
 document.addEventListener('DOMContentLoaded', (e) => {
-    const button = document.querySelector('--btn-download-webinarjam');
-    setupButton(button);
-
+    document.querySelector('.--btn-download-webinarjam').addEventListener('click', () => {
+        setupButton();
+    });
 })
 
-const setupButton = (element) => {
-    document.querySelector('button').addEventListener('click', () => {
-        chrome.tabs.query({
-            currentWindow: true,
-            active: true
-        }, (tabs) => {
-            chrome.tabs.sendMessage(tabs[0].id, 'is-end');
-        })
-        chrome.tabs.executeScript({
-            file: "main.js"
-        });
-    });
+function newTabVideo() {
+    const createPopUpMessage = (messsage, delay) => {
+        const popUp = document.createElement('div');
+        popUp.innerHTML = messsage;
+    
+        popUp.style.background = "#00fffb";
+        popUp.style.position = "fixed";
+        popUp.style.bottom = "-5rem";
+        popUp.style.left = "50%";
+        popUp.style.transform = "translateX(-50%)";
+        popUp.style.color = "#000";
+        popUp.style.padding = "1rem";
+        popUp.style.border = "3px solid #000";
+        popUp.style.fontWeight = "bold";
+        popUp.style.fontSize = "16px";
+        popUp.style.transition = "all 0.2s ease-in-out";
+        popUp.style.zIndex = "11111111";
+    
+        document.body.append(popUp);
+    
+        setTimeout(() => {
+            popUp.style.bottom = "2rem";
+        }, 100);
+    
+    
+        setTimeout(() => {
+            popUp.style.bottom = "-5rem";
+        }, delay);
+    
+        setTimeout(() => {
+            popUp.remove();
+        }, (delay + 200));
+    }
+
+    const isEnd = window.location.pathname.includes('replay')
+    if (isEnd) {
+        const video = document.getElementById("vjs_video_3_html5_api");
+        const el = document.createElement('textarea');
+
+        const src = video.src;
+
+        el.value = src;
+        document.body.appendChild(el);
+        el.select();
+        document.execCommand('copy');
+        document.body.removeChild(el);
+
+        window.open(src, "_blank");
+
+        createPopUpMessage('Odkaz byl uložen v clipboardu.', 3500)
+    } else {
+        createPopUpMessage('Video není připraveno.', 3500)
+    }
 }
+
+function checkDownload() {
+   
+}
+
+const executeScript = (tabId) =>{
+    chrome.scripting.executeScript({
+        target: {tabId: tabId, allFrames: false},
+        func: newTabVideo
+    }).then(() => console.log("Script injected")).catch((e)=> console.warn(e));
+}
+
+const setupButton = async() => {
+    const page = await getCurrentTab();
+    executeScript(page.id);
+}
+
+async function getCurrentTab() {
+    let queryOptions = { active: true };
+    let [tab] = await chrome.tabs.query(queryOptions);
+    return tab;
+  }
+
